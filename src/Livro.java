7b4ff421bffd9a0de7c79e1f5562b1b648d302c3
Vivi7-sys <uchiravitor2412@gmail.com>
2026-08-10 08:05:32 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -6,12 +6,9 @@ public class Livro{
 
 
     public Livro(){
-        titulo = "Revolução dos Bichos";
-        autor = "George Owell";
-        disponivel = true;
-        TotalLivros ++;
+        this("sem titulo", "sem autor");
     }
-    public Livro(String titulo, String autor, boolean disponivel){
+    public Livro(String titulo, String autor){
         this.titulo = titulo;
         this.autor =  autor;
         this.disponivel = disponivel;
